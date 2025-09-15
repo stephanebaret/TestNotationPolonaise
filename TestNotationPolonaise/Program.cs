5a011a2e37c9fcb06383474a261e2cadbb8300ea
Stéphane BARET <stephane.baret@live.fr>
2025-09-15 22:53:18 +0400

    Ajout de la fonction de calcul en polonaise

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -5,6 +5,123 @@ namespace TestNotationPolonaise
     class Program
     {
         /// <summary>
+        /// Calcul d'une formule en notation polonaise
+        /// </summary>
+        /// <param name="formule">La formule a calculer</param>
+        /// <returns>résultat du calcul</returns>
+        static double Polonaise(String formule)
+        {
+            double resultat = double.NaN;
+            String[] formuleSplit = formule.Split(' ');
+            double x, y;
+
+            int i = (formuleSplit.Length - 1);
+            while ( i >= 2)
+            {
+                int j = i;
+                while ((j > 0) && formuleSplit[j] != "+" && formuleSplit[j] != "-" && formuleSplit[j] != "*" && formuleSplit[j] != "/")
+                {
+                    Console.WriteLine("j = " + j);
+                    j--;
+                }
+                
+                try
+                {
+                    Console.WriteLine("formuleSplit[j + 2] = " + formuleSplit[j + 2]);
+                    x = double.Parse(formuleSplit[j + 2]);
+                }
+                catch
+                {
+                    return double.NaN;
+                }
+
+                try
+                {
+                    Console.WriteLine("formuleSplit[j + 1] = " + formuleSplit[j + 1]);
+                    y = double.Parse(formuleSplit[j + 1]);
+                }
+                catch
+                {
+                    return double.NaN;
+                }
+
+                Console.WriteLine("formuleSplit[j] = " + formuleSplit[j]);
+                switch (formuleSplit[j])
+                {
+                    case "+": resultat = y + x; 
+                        break;
+                    case "-": resultat = y - x;
+                        break;
+                    case "*": resultat = y * x;
+                        break;
+                    case "/":
+                        if (x == 0) {
+                            return double.NaN;
+                        }
+                        else
+                        {
+                           resultat = y / x;
+                            break;
+                        }
+                    default: return double.NaN;
+                }
+
+                for (int m = 0; m < formuleSplit.Length; m++)
+                {
+                    Console.Write(" " + formuleSplit[m]);
+                }
+                Console.WriteLine();
+
+                formuleSplit[j] = resultat.ToString();
+
+                int k;
+                for (k = j + 1; k < formuleSplit.Length - 2; k++)
+                {
+                    formuleSplit[k] = formuleSplit[k+2];
+                    formuleSplit[k + 2] = "";
+                    formuleSplit[k + 1] = "";
+                }
+                // formuleSplit[k] = "";
+                // formuleSplit[k - 1] = "";
+
+
+                for (int m = 0; m < formuleSplit.Length; m++)
+                {
+                    Console.Write(" " + formuleSplit[m]);
+                }
+                Console.WriteLine();
+
+                i = i - 2;
+                Console.WriteLine("i = " + i);
+            }
+
+            if (i + 1 == 0)
+            {
+                return double.NaN;
+            }
+
+            if (i + 1 == 1)
+            {
+                try
+                {
+                    x = double.Parse(formuleSplit[0]);
+                    resultat = x;
+                }
+                catch
+                {
+                    return double.NaN;
+                }
+            }
+
+            if (i + 1 == 2)
+            {
+                return double.NaN;
+            }
+
+            return resultat;
+        }
+
+        /// <summary>
         /// saisie d'une réponse d'un caractère parmi 2
         /// </summary>
         /// <param name="message">message à afficher</param>
